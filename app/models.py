8d9d59df0c393ea7092005dc9c39723382e50bb8
Artem Kovyryagin <dastet@rambler.ru>
2021-01-18 20:03:01 +0300

    add dependencies

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 from datetime import datetime
-from app import db
+from app import db, login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 
 recipe_ingredient = db.Table('recipe_ingredient',
@@ -9,7 +11,7 @@ recipe_ingredient = db.Table('recipe_ingredient',
                     db.Column('ingredient_id', db.Integer, db.ForeignKey('ingredient.id')))
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'user'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -20,6 +22,16 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    @login.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
 
 class Recipe(db.Model):
